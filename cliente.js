db64bd759ad8d5d2f654d3fe4de5b57caa3ea6c6
Matheus Morcinek <matheus.morcinek@Matheuss-MacBook-Pro.local>
2020-10-06 13:34:30 -0300

    exibindo dados dinamicamente com js

---
+++ cliente.js
@@ -0,0 +1,34 @@
+const informacoesClientes = [
+    {
+        cpf: 123456789,
+        nome: "Matheus"
+    },
+    {
+        cpf: 987654321,
+        nome: "Bruna"
+    },
+    {
+        cpf: 654321789,
+        nome: "Maria Fernanda"
+    }
+];
+
+const tabela = document.querySelector("[data-tabela]");
+
+const exibeCliente = (cpf, nome) => {
+
+    const linha = document.createElement('tr');
+
+    const conteudoLinha = `
+    <td>${cpf}</td>
+    <td>${nome}</td>
+    `;
+
+    linha.innerHTML = conteudoLinha;
+    return linha;
+};
+
+informacoesClientes.forEach(cliente => {
+
+    tabela.appendChild(exibeCliente(cliente.cpf, cliente.nome));
+});
